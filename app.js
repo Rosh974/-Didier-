acc19f28a19342a705c277f514cc832f413dd7a3
Rosh974 <didier.vilrus1@gmail.com>
2018-06-26 09:17:08 +0400

    ajout commentaire dans le code

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ app.use(express.static('public'));
 // utilisation des template ejs 
 app.set("view engine", "ejs");
 
-
-app.get('/', function(req, res) {
+// route principale qui affichera le nom et le prenom sur la page index via ejs
+app.get('/', function (req, res) {
     didier(res);
 
 });
 
-function didier (res){
-    res.render('index',{
-        prenom:'didier', nom:'VILRUS'
+// fonction qui retourne le nom et le prenom en paramètre
+function didier(res) {
+    res.render('index', {
+        prenom: 'didier', nom: 'VILRUS'
     })
 }
 
 
-
-// // port d'écoute
-app.listen(port, function() { 
+// port d'écoute
+app.listen(port, function () {
     console.log("Listening on port 8040")
 });
